1682a8d7ec35b02bc2c89d74779f58f9d556bd21
Disha <dishabh10@gmail.com>
2026-06-15 22:02:06 +0200

    Menu amélioration

--- mon-portfolio/src/composents/layout/Navbar.tsx
+++ mon-portfolio/src/composents/layout/Navbar.tsx
@@ -12,7 +12,16 @@ export default function Navbar() {
 
       <div className={styles.center}>
         <Link href="/#hero-section" className={styles.link}>Profil</Link>
-        <Link href="/#competences-portfolio" className={styles.link}>Compétences</Link>
+        <div className={styles.dropdown}>
+          <Link href="/#competences-portfolio" className={styles.link}>Compétences</Link>
+          <div className={styles.dropdownMenu}>
+            <div className={styles.dropdownInner}>
+              <Link href="/competences/c1-realiser" className={styles.dropdownLink}>C1 — Réaliser</Link>
+              <Link href="/competences/c2-optimiser" className={styles.dropdownLink}>C2 — Optimiser</Link>
+              <Link href="/competences/c6-collaborer" className={styles.dropdownLink}>C6 — Collaborer</Link>
+            </div>
+          </div>
+        </div>
         <Link href="/#contact" className={styles.link}>Contact</Link>
       </div>
     </nav>
